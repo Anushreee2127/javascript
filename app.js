5575011a4a8cb04591b96920441ba3b897aac106
Sai Gurunath Reddy <your@example.com>
2025-09-22 12:39:13 +0530

    changes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var a=67;
-console.log(a);
+a=40;
+console.log(typeof a);
+name='anu';
+console.log(typeof name);
+data=null;
+console.log(typeof data);
+d=undefined;
+console.log(typeof d);
+array=[23,"anu",true.null];
+console.log(typeof array);
+data={
+
+    name:"anu"
+};
+ 
---
+++ function.js
@@ -0,0 +1,18 @@
+// function add()
+// {
+// var a = 21;
+// var b = 23;
+// document.writeln(a+b);
+// console.log(a+b);
+// };
+// add();
+//ES7
+const add =(a,b) => console.log(a+b);
+add(78,90);
+let name = "anu";
+function demo()
+{
+    let name = "siva";
+    document.write("hello,My name is $(name)");
+
+}
